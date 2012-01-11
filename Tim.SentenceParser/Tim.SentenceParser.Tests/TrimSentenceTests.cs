729e603b64f9c29f868200b4038c5c049d2f0352
Tim Burkhart <burkhartt@digitalev.com>
2012-01-11 16:49:40 -0600

    Added a sentence parser

---
+++ Tim.SentenceParser/Tim.SentenceParser.Tests/TrimSentenceTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+
+namespace Tim.SentenceParser.Tests
+{
+    [TestFixture]
+    public class TrimSentenceTests
+    {
+        [Test]
+        public void When_I_am_given_an_empty_string_return_an_empty_string()
+        {
+            var emptyString = string.Empty;
+            var result = emptyString.TrimSentence(50);
+
+            result.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void When_the_length_is_longer_than_the_length_of_the_string_then_return_the_string()
+        {
+            var sentence = "This is a fairly long sentence.";
+
+            var result = sentence.TrimSentence(50000);
+
+            result.ShouldEqual(sentence);
+        }
+
+        [Test]
+        public void When_the_length_is_zero_then_return_an_empty_string()
+        {
+            var sentence = "A sentence.";
+
+            var result = sentence.TrimSentence(0);
+
+            result.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void When_the_length_is_in_the_middle_of_a_word_then_end_at_the_previous_word()
+        {
+            var sentence = "The big sea creature was nice.";
+
+            var result = sentence.TrimSentence(15);
+
+            result.ShouldEqual("The big sea");
+        }
+
+        [Test]
+        public void When_I_am_given_an_empty_string_with_the_ellipsis_option_return_an_empty_string()
+        {
+            var emptyString = string.Empty;
+            var result = emptyString.TrimSentence(50, true);
+
+            result.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void When_the_length_is_longer_than_the_length_of_the_string_and_there_is_the_ellipsis_option_then_return_the_string()
+        {
+            var sentence = "This is a fairly long sentence.";
+
+            var result = sentence.TrimSentence(50000, true);
+
+            result.ShouldEqual(sentence);
+        }
+
+        [Test]
+        public void When_the_length_is_zero_with_the_ellipsis_option_then_return_an_empty_string()
+        {
+            var sentence = "A sentence.";
+
+            var result = sentence.TrimSentence(0, true);
+
+            result.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void When_the_length_is_in_the_middle_of_a_word_and_the_ellipsis_is_wanted_then_end_at_the_previous_word_and_add_an_ellipsis()
+        {
+            var sentence = "The big sea creature was nice.";
+
+            var result = sentence.TrimSentence(15, true);
+
+            result.ShouldEqual("The big sea...");
+        }
+    }
+}
---
+++ Tim.SentenceParser/Tim.SentenceParser/SentenceParser.cs
@@ -0,0 +1,36 @@
+﻿namespace System
+{
+    public static class SentenceParser
+    {
+        public static string TrimSentence(this string @string, int length)
+        {
+            if (length <= 0 || string.IsNullOrEmpty(@string)) return string.Empty;
+            if (length >= @string.Length) return @string;
+
+            if (TheNextCharacterIsEmpty(@string, length)) return @string.Substring(0, length);
+
+            return TheSentenceEndingAfterThePreviousWord(@string, length);
+        }
+
+        public static string TrimSentence(this string @string, int length, bool addEllipsis)
+        {
+            return TrimSentence(@string, length) + (ShouldAddAnEllipsis(addEllipsis, @string, length) ? "..." : string.Empty);
+        }
+
+        private static bool ShouldAddAnEllipsis(bool addEllipsis, string @string, int length)
+        {
+            return addEllipsis && @string.Length > length && length > 0;
+        }
+
+        private static string TheSentenceEndingAfterThePreviousWord(string @string, int length)
+        {
+            var lastSpaceIndex = @string.Substring(0, length).LastIndexOf(" ");
+            return @string.Substring(0, lastSpaceIndex);
+        }
+
+        private static bool TheNextCharacterIsEmpty(string @string, int length)
+        {
+            return @string[length + 1] == ' ';
+        }
+    }
+}
